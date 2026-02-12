17329d80ec2729213f9e57ded9cf0fcd0b4f0f4a
HeavyHope <heavyhope52@gmail.com>
2026-02-12 15:20:16 +0300

    fix: resolve TC-02 and TC-04, add --logs help flag

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,20 +7,34 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f",'--file',help='input file name with logs',
                     default = 'dates.log')
 parser.add_argument('-d','--detail',help = 'show all logs',action='store_true')
+parser.add_argument('--logs',action='store_true',help='show correct logs lvl')
 args = parser.parse_args()
 logs_file = args.file
 
-
+if args.logs: # check info flag
+    print(f"\nExample of correct log format and levels: ")
+    print(
+            f"2026-02-12 12:59:03 | WARN  | High CPU usage\n"
+            f"2026-02-12 13:14:41 | DEBUG | The connection speed is good\n"
+            f"2026-02-12 13:16:22 | INFO  | User logged out\n"
+            f"2026-02-12 13:31:32 | ERROR | File not found\n"
+            f"2026-02-12 13:56:01 | FATAL | Program crash\n"
+            )
+    exit()
 
 file_path = Path(logs_file)
 
 logs = []
 if file_path.exists():
     if file_path.is_file():
-        with open(file_path, 'r') as file:
-            for i in file:
-                if i.strip():
-                    logs.append(i)
+        try:
+            with open(file_path, 'r') as file:
+                for i in file:
+                    if i.strip():
+                        logs.append(i)
+        except UnicodeDecodeError:
+            print('[Error] Unsupported file encoding or binary format')
+            exit()
 
 else: 
     print('«Error: Path is not a file»')
@@ -32,16 +46,21 @@ def count_logs(logs):
     count_values = {"DEBUG":0, "INFO":0, "WARN":0,
                     "ERROR":0, "FATAL":0, "UNKNOWN": 0}
     for log_lvl in logs:
-        if "DEBUG" in log_lvl: count_values["DEBUG"] += 1
-        elif "INFO" in log_lvl: count_values["INFO"] += 1
-        elif "WARN" in log_lvl: count_values["WARN"] += 1
-        elif "ERROR" in log_lvl: count_values["ERROR"] += 1
-        elif "FATAL" in log_lvl: count_values["FATAL"] += 1
+        if "| DEBUG" in log_lvl: count_values["DEBUG"] += 1
+        elif "| INFO" in log_lvl: count_values["INFO"] += 1
+        elif "| WARN" in log_lvl: count_values["WARN"] += 1
+        elif "| ERROR" in log_lvl: count_values["ERROR"] += 1
+        elif "| FATAL" in log_lvl: count_values["FATAL"] += 1
         else: count_values['UNKNOWN'] += 1
     print(total)
     for i in count_values:
         if count_values[i] != 0:
             print(f"{i.ljust(7)} : {count_values[i]}")
+    if count_values["UNKNOWN"] != 0:
+        print(f"\nPlease check your formats logs. "
+            f"Found {count_values['UNKNOWN']} lines with UNKNOWN format")
+        print(f"Use -h or --logs to see the required log format")
+        exit()
     
     with open("report.txt", 'w') as f: # create statistics file
         f.write(f"{total.strip()}\n\n")
